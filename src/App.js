3b171380199ff78f7b533c42b78e7eed8f5cd829
blasecato <bl.calderon@udla.edu.com>
2019-08-11 21:13:31 -0500

    frontend inicio

--- src/App.js
+++ src/App.js
@@ -1,24 +1,88 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/images/cocinera.png';
+import shef from './assets/images/shef.jpg';
 import './App.css';
+import { Layout,Row,Col,Button,Carousel } from 'antd';
+import 'antd/dist/antd.css';
+import { Card } from 'antd';
+
+const { Meta } = Card;
+const { Header, Footer, Sider, Content } = Layout;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+          
+      <Layout className="lay">
+        
+      <Content className="content">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header className="head">
+      <center>
+        <Row gutter={4}>
+          <Col span={4}>
+          <a className="a">
+            Inicio
+          </a>
+          </Col>
+          <Col span={4}>
+          <a className="a" href="https://softwarecrafters.io/react/tutorial-react-js-introduccion">
+            Tienda
+          </a>
+          </Col>
+          <Col span={5}>
+          <a className="a">
+            Contacto
+          </a>
+          </Col>
+          <Col span={5}>
+          <a className="a">
+            Registrarse
+          </a>
+          </Col>
+          <Col span={5}>
+          <a className="a">
+            Iniciar Sesion
+          </a>
+          </Col>
+        </Row>
+      </center>
+      </Header>
+          <Row gutter={16}>
+              <Col span={8} className="card2">
+                <img src={shef} className="card2" alt="logo" />
+              </Col>
+              <Col span={8}>
+                <Card  className="card1">
+                  <h1 className="title">PRODUCTOS</h1>
+                  <h5 className="tex">Visita nuestra tienda y reserva tu pedido.</h5>
+                  <br/>
+                  <Button className="Button">Ver Tienda</Button>
+                </Card>
+              </Col>
+          </Row>
+        
+        <Carousel autoplay className="ant-carousel">
+            <div>
+              <h3>1</h3>
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+        </Carousel>
+        <br/>
+      </Content>
+      <Footer className="footer">
+
+      </Footer>
+      </Layout>
+      
     </div>
   );
 }
