d999ae74c11854ff3567d41d8d84d3d75f69a109
blasecato <bl.calderon@udla.edu.com>
2019-08-25 03:00:44 -0500

    concceion con el backend y consumir servicios desde mon-backend

--- src/App.js
+++ src/App.js
@@ -2,29 +2,19 @@ import React from 'react';
 import 'antd/dist/antd.css';
 import Home from '../src/scenes/Home/Home';
 import Login from '../src/scenes/Login/Login';
-import LayoutHome from '../src/layouts/LayoutHome/LayoutHome';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import { Layout,Row,Col,Button,Carousel } from 'antd';
-import 'antd/dist/antd.css';
-import { Menu, Breadcrumb,Icon } from 'antd';
+import { BrowserRouter as Router, Route} from "react-router-dom";
 import Register from '../src/scenes/Register/Register';
 
-const { SubMenu } = Menu;
 
 function App() {
   return (
     <div className="App">
-            <Router>
-            
-            <Route path="/" exact component={Home} />
-            <Route path="/home/" component={Home} />
-            <Route path="/login/" component={Login} />
-            <Route path="/register/" component={Register} />
-            </Router>
-    
-    
-    
-   
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/home/" component={Home} />
+        <Route path="/login/" component={Login} />
+        <Route path="/register/" component={Register} />
+      </Router>
     </div>
   );
 }
